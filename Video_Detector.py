d96862a741117f3a5ea16959adfe83fe60b7672e
suase1 <suase9303@gmail.com>
2017-03-02 18:29:59 +0900

    first commit

--- Video_Detector.py
+++ Video_Detector.py
@@ -1,17 +1,17 @@
 import cv2
 import sys
-from contourimage import drawcontour_
+from Video_CharsRecognizer import drawcontour_
 import numpy as np
 
 
-
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture('video1.mp4')
+#video_capture = cv2.VideoCapture(0)
 
 while True:
     ret, frame = video_capture.read()
 
     drawcontour_(frame)
-
+    cv2.namedWindow('video', cv2.WINDOW_NORMAL)
     cv2.imshow('video',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
